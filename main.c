b694596683fda7c4f691982abe9d8439ab94c475
aaronpatton <aaronrpatton@gmail.com>
2022-01-16 18:25:40 -0700

    initial files for convolution algorithm

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+#define SIG_LENGTH 320
+
+
+extern double InputSignal_f32_1kHz_15kHz[SIG_LENGTH];
+
+
+
+
+int main()
+{
+
+}
+
+
+
+
+
